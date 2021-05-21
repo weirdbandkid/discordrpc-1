b1ceaabe5448de9dd23a078b75d160388c3444b8
weirdbandkid <Chicagofirefan05@outlook.com>
2021-05-21 16:13:48 -0400

    fixed CD command

--- DiscordRPC.bat
+++ DiscordRPC.bat
@@ -2,5 +2,5 @@ REM This is for Windows only!!
 @echo off
 color 1
 REM You must chage the location for this to work (you can remove anything from REM to here)
-cd cd C:\User\Location
+cd C:\User\Location
 node .
